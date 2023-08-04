09906912a0e1a8bf159d7f16cf1a9cb11e02b160
Guilherme <guilhermeosilva12@gmail.com>
2023-08-04 15:08:33 -0300

    mudei nada

--- Projeto/app.js
+++ Projeto/app.js
@@ -3,12 +3,34 @@ var app = angular.module('conversorMoeda', []);
 
 app.controller('conversorController', function($scope, $http) {
     
+    // $scope.SiglasMoeda = function() {
+    //     var url="https://api.apilayer.com/currency_data/list"
+
+    //     var headers = {
+    //         "apikey": "6FFJUzjNmg3giqhHFd0ZTpUTcdjx1n9q"
+    //     };
+
+    //     $http({
+    //         method: 'GET',
+    //         url: url,
+    //         headers: headers
+    //     }).then(function(response) {
+    //         console.log(response.data.currencies)
+    //         $scope.moedas = response.data.currencies
+    //     }, function(error) {
+    //         console.log('error', error);
+    //     });
+
+
+    // };
+    // $scope.SiglasMoeda();
+    
+    //1.conversão de moedas
     $scope.fromCurrency = 'USD';
     $scope.toCurrency = 'BRL';
     $scope.amount = 1;
     $scope.result = null;
 
-    //1.conversão de moedas
     $scope.convert = function() {
         var url = "https://api.apilayer.com/currency_data/convert";
         url += "?to=" + $scope.toCurrency;
@@ -24,7 +46,7 @@ app.controller('conversorController', function($scope, $http) {
             url: url,
             headers: headers
         }).then(function(response) {
-            // Aqui, você precisa adaptar de acordo com a resposta da API
+            console.log(response)
             $scope.result = response.data.result;
         }, function(error) {
             console.log('error', error);
