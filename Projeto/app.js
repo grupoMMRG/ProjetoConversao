9642aa02e9980da17a910f8d874b8932ea55e171
Guilherme <guilhermeosilva12@gmail.com>
2023-10-20 12:05:14 -0300

    [WIP] work in progress

--- Projeto/app.js
+++ Projeto/app.js
@@ -2,6 +2,34 @@ var app = angular.module('conversorMoeda', []);
 
 app.controller('conversorController', function($scope, $http) {
     
+    $scope.noticia_inv = function() {
+        let moedas = ['IBOV.SA', 'USD', 'BTC', 'UE', 'BR','CNY', 'L'];
+        
+        $scope.quotes = [];
+
+        moedas.forEach(function(moeda) {
+            var url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${moeda}&apikey=X25X5M5L6OJAITS2`;
+        
+            $http({
+                method: 'GET',
+                url: url,
+            }).then(function(response) {
+                console.log("Resposta para moeda:", moeda, response.data); // Esta linha foi adicionada
+                if(response.data['Global Quote']) {
+                    $scope.quotes.push(response.data['Global Quote']);
+                }
+            }, function(error) {
+                console.log('error', error);
+            });
+        });
+        
+    };
+
+    $scope.noticia_inv();
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     $scope.SiglasMoeda = function() {
         var url="https://api.apilayer.com/currency_data/list"
 
@@ -66,104 +94,59 @@ app.controller('conversorController', function($scope, $http) {
     
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    $scope.fromMoeda = 'USD';
-    $scope.toMoeda = 'BRL';
-    $scope.quotes = null;
+// Obter a data de hoje
+const hoje = new Date();
 
-    $scope.taxa = function() {
-        var url = "https://api.apilayer.com/currency_data/live";
-        url += "?source=" + $scope.toMoeda;
-        url += "&currencies=" + $scope.fromMoeda;
+// Subtrair 7 dias
+hoje.setDate(hoje.getDate() - 14);
 
+// Obter o dia, mês e ano da data resultante
+const dia = hoje.getDate();
+const mes = hoje.getMonth() + 1; // Os meses são base 0, então somamos 1
+const ano = hoje.getFullYear();
 
-        var headers = {
-            "apikey": "r60LyyS7OgcUADI4XUyfl0iQEdIFE4hT"
-        };
+// Formatar a data no formato desejado (MM-DD-AAAA)
+const dataFormatada = `${mes}-${dia}-${ano}`;
 
-        $http({
-            method: 'GET',
-            url: url,
-            headers: headers
-        }).then(function(response) {
-            console.log(response)
-            $scope.quotes = response.data.quotes;
-            console.log($scope.quotes)
-        }, function(error) {
-            console.log('error', error);
-        });
-    };
+console.log(dataFormatada);
 
-    $scope.taxa();
+$scope.taxa = function() {
+    var url = `http://servicodados.ibge.gov.br/api/v3/noticias/?de=${dataFormatada}`;
 
-    // Botao com bug, testa ai
+    const BASE_URL = "http://agenciadenoticias.ibge.gov.br/";
 
-    $scope.inverterT = function() {
-        var tempe = $scope.fromMoeda;
-        $scope.fromMoeda = $scope.toMoeda;
-        $scope.toMoeda = tempe;
-        
+    $http({
+        method: 'GET',
+        url: url
+    }).then(function(response) {
+        if (response.data.items.length >= 8) {
+            let noticiasSelecionadas = [];
     
-        $scope.convert();
-    };
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //funcao (Comparar a cotação do Real frente as outras moedas nos últimos X dias.)
-    var today = new Date();
-    $scope.dataComparacao = null;
-
-    $scope.FuncData = function(data) {
-        if (data) {  // Verifica se 'data' está definida
-            let dataObj = new Date(data);
-            var dia = String(dataObj.getDate()).padStart(2, '0');
-            var mes = String(dataObj.getMonth() + 1).padStart(2, '0');
-            var ano = dataObj.getFullYear();
-            var dataFinal = ano + '-' + mes + '-' + dia;
-            return dataFinal;
+            while (noticiasSelecionadas.length < 8) {
+                let num_noticia = Math.floor(Math.random() * response.data.items.length);
+                if (!noticiasSelecionadas.includes(num_noticia)) {
+                    noticiasSelecionadas.push(num_noticia);
+                }
+            }
+    
+            // Converta o campo 'imagens' de string para objeto
+            $scope.noticias = noticiasSelecionadas.map(index => {
+                let noticia = response.data.items[index];
+                noticia.imagens = JSON.parse(noticia.imagens.replace(/\\/g, ''));
+                noticia.imagens.image_intro = BASE_URL + noticia.imagens.image_intro; // Complete a URL da imagem
+                return noticia;
+            });
+    
         } else {
-            return null;
+            console.log('Não existem 4 notícias disponíveis no período fornecido.');
         }
-    }
+        
+    }, function(error) {
+        console.log('error', error);
+    });
+};
 
-    
-    
-    $scope.Comparacao1 = function() {
-        var url = "https://api.exchangeratesapi.io/history"
-        $scope.$watch('dataComparacao', function(newVal) {
-            if (newVal) {
-                var data = $scope.FuncData(newVal)
-                url += "?start_at=" + data
-            }
-        });
-        url += "days_ago&end_at=" + $scope.FuncData(today)
-        $scope.$watch('fromMoedaComparacao1', function(newVal) {
-            if (newVal) {
-                var moeda = $scope.FuncData(newVal)
-                url += "&base=BRL&symbols=" + moeda
-            }
-        });
-
-        var headers = {
-            "apikey": "8ab06a190663d017ecd12752080bd44d"
-        };
-
-        $http({
-            method: 'GET',
-            url: url,
-            headers: headers
-        }).then(function(response) {
-            console.log(response.data)
-            $scope.result1 = response.data
-        }, function(error) {
-            console.log('error', error);
-        });
-    }
-
-    $scope.fromMoedaComparacao1 = 'USD';
-    $scope.resultComparacao1 = null;
-
-    console.log($scope.fromMoedaComparacao1)
-    $scope.Comparacao1()
+$scope.taxa();
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
